ea9730169783fbec2621beb02d21f1f0dae31174
servanh <servandobob95@gmail.com>
2019-11-06 19:09:10 -0600

    redirected home page to home.html template

--- blogsite/views.py
+++ blogsite/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html', context=request)
+    return render(request, 'home.html', context=Context)
 
 
 def individual_post(request):
