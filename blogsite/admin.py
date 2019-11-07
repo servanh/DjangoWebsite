eb9bd956218ddda670227f4b128ec072a5047299
root <root@vps.server.local>
2019-11-07 00:44:59 +0000

    updating server changes

--- blogsite/admin.py
+++ blogsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from blogsite.models import Post
+from blogsite.models import Comment
 # Register your models here.
+
+
+admin.site.register(Post)
+admin.site.register(Comment)
